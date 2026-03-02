30b5d2aa5534eb33db53989ac17b1032f726b001
Ludvig Erdmann <ludvig.erdmann@gmail.com>
2026-03-03 00:21:04 +0100

    pwa

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 const main = document.querySelector('main')
 const content = document.querySelector('#content')
+const bg = document.querySelector('#bg')
 
 main.addEventListener('scroll', () => {
     content.style.setProperty('--scroll', `${main.scrollTop}`)
+    bg.style.setProperty('--content-scroll', `${main.scrollTop}`)
 })
---
+++ js/touch-effect.js
@@ -0,0 +1,38 @@
+const drag_effects = document.querySelectorAll('[draggable]')
+const drag_intensity = .008
+const size_decrease = .98
+
+drag_effects.forEach(e => {
+    const wrapper = document.createElement('div');
+    wrapper.className = 'drag-wrapper';
+
+    e.replaceWith(wrapper);
+    wrapper.appendChild(e);
+
+    wrapper.addEventListener('touchstart', (event) => {
+        const touch = event.touches[0]
+
+        wrapper.dataset.startX = touch.clientX
+        wrapper.dataset.startY = touch.clientY
+        wrapper.classList.add('dragging')
+        wrapper.style = `transform: scale(${size_decrease});`
+    })
+
+    wrapper.addEventListener('touchend', (event) => {
+        wrapper.classList.remove('dragging')
+        wrapper.style = ``
+    });
+});
+
+document.addEventListener('touchmove', (event) => {
+    event.preventDefault();
+
+    document.querySelectorAll('.dragging').forEach(e => {
+        const touch = event.touches[0]
+
+        const x = (touch.clientX - e.dataset.startX) * drag_intensity
+        const y = (touch.clientY - e.dataset.startY) * drag_intensity
+        e.style = `transform: translate(${x}px, ${y}px) scale(${size_decrease});`
+    });
+        
+    }, { passive: false });
