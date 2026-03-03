fd8eda5fc4587a13a6351a0b632506ed903f2531
Ludvig Erdmann <ludvig.erdmann@gmail.com>
2026-03-03 20:56:47 +0100

    small fixes

--- js/touch-effect.js
+++ js/touch-effect.js
@@ -4,25 +4,41 @@ const size_decrease = .96
 const max_stretch = 12000
 
 drag_effects.forEach(e => {
-    const wrapper = document.createElement('div');
-    wrapper.className = 'drag-wrapper';
+    if (e.getAttribute('draggable') === 'wrapper') {
+        const wrapper = document.createElement('div');
+        wrapper.className = 'drag-wrapper';
 
-    e.replaceWith(wrapper);
-    wrapper.appendChild(e);
+        e.replaceWith(wrapper);
+        wrapper.appendChild(e);
 
-    wrapper.addEventListener('touchstart', (event) => {
-        const touch = event.touches[0]
+        wrapper.addEventListener('touchstart', (event) => {
+            const touch = event.touches[0]
 
-        wrapper.dataset.startX = touch.clientX
-        wrapper.dataset.startY = touch.clientY
-        wrapper.classList.add('dragging')
-        wrapper.style = `transform: scale(${size_decrease});`
-    })
+            wrapper.dataset.startX = touch.clientX
+            wrapper.dataset.startY = touch.clientY
+            wrapper.classList.add('dragging')
+            wrapper.style = `transform: scale(${size_decrease});`
+        })
 
-    wrapper.addEventListener('touchend', (event) => {
-        wrapper.classList.remove('dragging')
-        wrapper.style = ``
-    });
+        wrapper.addEventListener('touchend', (event) => {
+            wrapper.classList.remove('dragging')
+            wrapper.style = ``
+        });
+    } else {
+        e.addEventListener('touchstart', (event) => {
+            const touch = event.touches[0]
+
+            e.dataset.startX = touch.clientX
+            e.dataset.startY = touch.clientY
+            e.classList.add('dragging')
+            e.style = `transform: scale(${size_decrease});`
+        })
+
+        e.addEventListener('touchend', (event) => {
+            e.classList.remove('dragging')
+            e.style = ``
+        });
+    }
 });
 
 document.addEventListener('touchmove', (event) => {
