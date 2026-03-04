b23bd051ce31c4be77a9007ff3efee3510565ff0
Ludvig Erdmann <ludvig.erdmann@gmail.com>
2026-03-04 22:18:04 +0100

    added stat updates

--- js/index.js
+++ js/index.js
@@ -2,7 +2,21 @@ const main = document.querySelector('main')
 const content = document.querySelector('#content')
 const bg = document.querySelector('#bg')
 
-main.addEventListener('scroll', () => {
-    content.style.setProperty('--scroll', `${main.scrollTop}`)
-    bg.style.setProperty('--content-scroll', `${main.scrollTop}`)
+window.addEventListener('scroll', () => {
+    console.log('aa')
+    content.style.setProperty('--scroll', `${window.scrollY}`)
+    bg.style.setProperty('--content-scroll', `${window.scrollY}`)
 })
+
+
+const date_display = document.querySelector('#date')
+
+function updateDate() {
+    const days = ['Søndag','Mandag','Tirsdag','Onsdag','Torsdag','Fridag','Lørdag'];
+    const months = ['Januar','Februar','Marts','April','Maj','Juni','Juli','August','September','November','December'];
+    const now = new Date()
+    date_display.textContent = `${days[now.getDay()]} d. ${now.getDate()} ${months[now.getMonth()]}`
+}
+
+setTimeout(updateDate, 60000);
+updateDate()
